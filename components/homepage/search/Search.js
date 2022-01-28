af8999a29d02664bd29c92a818baf06da6103439
d4nielj <d4niel.djm@gmail.com>
2022-01-28 18:30:00 -0500

    Fetch succesfully artists from API

--- components/homepage/search/Search.js
+++ components/homepage/search/Search.js
@@ -1,4 +1,24 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+
+// must return a list of artists and a lists of albums ?
+
+const search = async (query, types) => {
+  const headers = {
+    Accept: '*/*',
+    'User-Agent': 'Daniel Jaramillo (https://www.d4nielj.me)',
+    Authorization: 'Discogs token=vudycwjnRceFkYrIQljEPPuBWzVtDVvNHAmPOLhi',
+  };
+
+  const response = await fetch(
+    `https://api.discogs.com/database/search?q=${query}&type=${types[0]}`,
+    {
+      headers,
+      method: 'GET',
+    }
+  );
+
+  return response;
+};
 
 const Search = () => {
   const [artists, setArtists] = useState([]);
@@ -7,17 +27,28 @@ const Search = () => {
 
   const handleQueryChange = (e) => {
     setQuery(e.target.value);
-    console.log(query);
   };
 
   const cleanSearch = () => {
     setQuery('');
   };
 
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    const res = await search(query, ['artist', 'album']);
+    const data = await res.json();
+    console.log(data);
+  };
+
   return (
     <div>
-      <input onChange={handleQueryChange} type='text' value={query} />
-      <button onClick={cleanSearch}>Clean</button>
+      <form onSubmit={handleSearch}>
+        <input onChange={handleQueryChange} type='text' value={query} />
+        <button type='submit'>Search</button>
+        <button type='button' onClick={cleanSearch}>
+          Clean
+        </button>
+      </form>
     </div>
   );
 };
