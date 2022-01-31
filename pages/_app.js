cbd5df24d9a841460bae99dccfbe5725870593ca
d4nielj <d4niel.djm@gmail.com>
2022-01-31 18:16:54 -0500

    Add Chakra-UI dependencies

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+import { ChakraProvider } from '@chakra-ui/react';
+
+const MyApp = ({ Component, pageProps }) => (
+  <ChakraProvider>
+    <Component {...pageProps} />;
+  </ChakraProvider>
+);
 
 export default MyApp;
