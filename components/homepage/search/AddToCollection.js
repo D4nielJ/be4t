05cbc9c4addf2ebbcf72db68ba032557e29d8bf0
d4nielj <d4niel.djm@gmail.com>
2022-02-02 22:29:33 -0500

    Clean AddToCollection

--- components/homepage/search/AddToCollection.js
+++ components/homepage/search/AddToCollection.js
@@ -1,25 +1,39 @@
-import React from 'react';
+import { Box } from '@chakra-ui/react';
+import React, { useState } from 'react';
 import fetchApi from '../../../lib/fetchApi';
 
 const getReleaseId = async (type, id) => {
-  const { data, error } = await fetchApi(`${type}s/${id}`);
+  const { data } = await fetchApi(`${type}s/${id}`);
   const { main_release: releaseId } = data;
   return releaseId;
 };
 
 const AddToCollection = ({ type, id, children }) => {
+  const [disabled, setDisabled] = useState(false);
+
   const handleAddToCollection = async () => {
+    setDisable(true);
     let releaseId = id;
     if (type !== 'release') {
       releaseId = await getReleaseId(type, id);
     }
-    const { data, error } = await fetchApi(
+    await fetchApi(
       `users/d4nielj/collection/folders/1/releases/${releaseId}`,
       'POST'
     );
-    console.log(data);
+    setDisable(false);
   };
-  return <div onClick={handleAddToCollection}>{children}</div>;
+
+  return (
+    <Box
+      type='button'
+      onClick={handleAddToCollection}
+      _disabled={{ opacity: 0.3 }}
+      disabled={disabled}
+    >
+      {children}
+    </Box>
+  );
 };
 
 export default AddToCollection;
